9e2a923a81b626232fe04c5968c77629faeb8941
ancoto1234 <mqan06@gmail.com>
2025-10-03 15:16:51 -0700

    Create main.java

---
+++ ArkanoidGame/src/main.java
@@ -0,0 +1,11 @@
+import core.*;
+
+public class main {
+    public static void main(String[] args) {
+            
+        Renderer renderer = new DefaultRenderer();
+        renderer.createWindow();
+
+    }
+    
+}
