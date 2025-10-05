8d3348c5d894fb501ee0d27734e9a87ea0a13e8f
nguyenmanh6911 <nmanh6911@gmail.com>
2025-10-05 22:28:48 +0700

    feat: add code base for Ball, GameObject, MoveableObject, Paddle

--- ArkanoidGame/src/core/DefaultRenderer.java
+++ ArkanoidGame/src/core/DefaultRenderer.java
@@ -4,11 +4,13 @@ import java.awt.Color;
 import javax.swing.*;
 
 public class DefaultRenderer extends JFrame implements Renderer {
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 600;
     public void createWindow() {
         setBackground(Color.BLACK);
         setFocusable(true);
         JFrame frame = new JFrame("Arkanoid Game");
-        frame.setSize(800, 600);
+        frame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setResizable(false);
--- ArkanoidGame/src/objects/Paddle.java
+++ ArkanoidGame/src/objects/Paddle.java
@@ -1,5 +1,43 @@
 package objects;
 
-public class Paddle {
-    
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class Paddle extends MoveableObject {
+    private final int speed;
+
+    public Paddle(int x, int y, int width, int height, Image image, int speed) {
+        super(x, y, width, height, image, 0, 0);
+        this.speed = speed;
+    }
+
+    public void move_Left() {
+        this.x -= speed;
+
+    }
+    public void move_Right () {
+        this.x += speed;
+    }
+
+    public void move_KeyBoard(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            move_Left();
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+            move_Right();
+        }
+    }
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    public void appyPowerUp() {
+
+    }
 }
