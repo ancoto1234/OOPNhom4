6651a53408384b0aa5d045877f1bbd47935ce684
thanhhuong1505 <tranhuong15052006@gmail.com>
2025-10-05 22:52:17 +0700

    feat(sound, objects): add base classes for Sound and Brick types

--- ArkanoidGame/src/objects/Brick.java
+++ ArkanoidGame/src/objects/Brick.java
@@ -1,5 +1,24 @@
 package objects;
 
-public class Brick {
-    
+import java.awt.*;
+
+public abstract class Brick extends GameObject {
+    protected int hitPoints;
+    protected String type;
+
+    public Brick(int positionX, int positionY, int width, int height, Image image, int hitPoints, String type) {
+        super(positionX, positionY, width, height, image);
+        this.hitPoints = hitPoints;
+        this.type = type;
+    }
+
+    public void takeHits (Ball ball) {
+        if (this.checkCollision(ball)) {
+            this.hitPoints--;
+        }
+    }
+
+    public boolean isDestroyed () {
+        return hitPoints == 0;
+    }
 }
