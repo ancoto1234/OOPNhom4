9cf078986b81cb4edf9be3e9194de1bc1790129e
ancoto1234 <mqan06@gmail.com>
2025-10-14 13:14:52 -0700

    feat: update Level

--- ArkanoidGame/src/level/Level1.java
+++ ArkanoidGame/src/level/Level1.java
@@ -13,15 +13,15 @@ public class Level1 extends Level {
     protected void buildLevel() {
         int startX = 50;
         int startY = 80;
-        int brickWidth = 55;
-        int brickHeight = 20;
+        int brickWidth = 40;
+        int brickHeight = 40;
         int rows = 5;
         int cols = 11;
 
         for (int r = 0;r < rows; r++) {
             for (int c = 0; c < cols; c++) {
-                int x = startX + c * (brickWidth + 5);
-                int y = startY + r * (brickHeight + 5);
+                int x = startX + c * (brickWidth);
+                int y = startY + r * (brickHeight);
                 Brick brick = new Brick(x, y, brickWidth, brickHeight, 1);
                 brick.setImage(gm.getImage("brick"));
                 bricks.add(brick);
