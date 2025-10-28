d87a46f5199762462b4fcee9a98124d13f13ef12
ancoto1234 <mqan06@gmail.com>
2025-10-28 04:31:40 -0700

    feat: update EndGamePanel and MenuPanel

--- ArkanoidGame/src/menu/MenuPanel.java
+++ ArkanoidGame/src/menu/MenuPanel.java
@@ -1,10 +1,11 @@
 package menu;
 
+import UI.Button;
 import core.MenuManager;
 import java.awt.*;
-import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class MenuPanel extends JPanel {
@@ -18,8 +19,8 @@ public class MenuPanel extends JPanel {
         setLayout(new GridBagLayout());
 
         try {
-            // backgroundImage = ImageIO.read(new File("ArkanoidGame/assets/menu_background.png"));
-            // logoImage = ImageIO.read(new File("ArkanoidGame/assets/logo.png"));
+            backgroundImage = ImageIO.read(new File("ArkanoidGame/assets/background.png"));
+            logoImage = ImageIO.read(new File("ArkanoidGame/assets/logo.png"));
 
             arcadeFont = Font.createFont(Font.TRUETYPE_FONT, new File("ArkanoidGame/assets/font.ttf")).deriveFont(30f);
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
@@ -33,75 +34,23 @@ public class MenuPanel extends JPanel {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(15, 15, 15, 15);
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridx = 0;
         gbc.gridy = 0;
 
-        JButton selectLevelButton = createMenuButton("Level", e -> toggleLevelPanel());
-        add(selectLevelButton, gbc);
-        gbc.gridy += 1;
+        Button startButton = new Button("Start Game");
+        Button exitButton = new Button("Exit");
 
-        JButton startButton = createMenuButton("Start Game", e -> toggleLevelPanel());
-        JButton exitButton = createMenuButton("Exit", e -> System.exit(0));
+        startButton.addActionListener(e -> startSelectedLevel(1));
+        exitButton.addActionListener(e -> System.exit(0));
 
         add(startButton, gbc);
         gbc.gridy += 1;
         add(exitButton, gbc);
 
-        gbc.gridy += 1;
-        levelPanel = new JPanel();
-        levelPanel.setOpaque(false);
-        levelPanel.setVisible(false);
-        levelPanel.setLayout(new GridLayout(1, 3, 10, 10));
-
-        JButton level1 = createMenuButton("Level 1", e -> startSelectedLevel(1));
-        JButton level2 = createMenuButton("Level 2", e -> startSelectedLevel(2));
-        JButton level3 = createMenuButton("Level 3", e -> startSelectedLevel(3));
-
-
-        levelPanel.add(level1);
-        levelPanel.add(level2);
-        levelPanel.add(level3);
-
-        add(levelPanel, gbc);
     }
 
-    private void toggleLevelPanel() {
-        levelPanel.setVisible(!levelPanel.isVisible());
-        revalidate();
-        repaint();
-    }
 
     private void startSelectedLevel(int level) {
         manager.startGame(level);
-    }
-
-    private JButton createMenuButton(String text, ActionListener action) {
-        JButton button = new JButton(text);
-
-        button.setFont(arcadeFont);
-        button.setForeground(Color.BLACK);
-
-        button.setFocusPainted(false);
-        button.setContentAreaFilled(false);
-        button.setOpaque(true);
-        button.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        button.setBorderPainted(false);
-
-        button.addActionListener(action);
-
-        button.addMouseListener(new MouseAdapter() {
-            public void mouseEntered(MouseEvent e) {
-                button.setForeground(Color.BLUE);
-                button.setFont(arcadeFont.deriveFont(34f));
-            }
-
-            public void mouseExited(MouseEvent e) {
-                button.setForeground(Color.BLACK);
-                button.setFont(arcadeFont.deriveFont(30f));
-            }
-        });
-
-        return button;
     }
 
     @Override
@@ -112,8 +61,7 @@ public class MenuPanel extends JPanel {
         }
 
         if (logoImage != null) {
-            int logoWidth = getWidth() / 2 - logoImage.getWidth() / 2;
-            g.drawImage(logoImage, logoWidth, 60,null);
+            g.drawImage(logoImage, 100, -30,600,400, null);
         }
     }
 }
