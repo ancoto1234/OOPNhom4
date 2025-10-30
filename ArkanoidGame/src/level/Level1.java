db39182477df6f3c3da42b37e9c54474e73c2b9c
ancoto1234 <mqan06@gmail.com>
2025-10-30 01:44:20 -0700

    feat: build level

--- ArkanoidGame/src/level/Level1.java
+++ ArkanoidGame/src/level/Level1.java
@@ -1,9 +1,12 @@
 package level;
 
 import core.GameManager;
+import java.util.Random;
 import objects.*;
 
 public class Level1 extends Level {
+
+    private static final Random random = new Random();
 
     public Level1(GameManager gm) {
         super(gm);
@@ -11,20 +14,43 @@ public class Level1 extends Level {
 
     @Override
     protected void buildLevel() {
-        int startX = 100;
-        int startY = 50;
-        int brickWidth = 42;
-        int brickHeight = 20;
-        int rows = 16;
-        int cols = 16;
+        int startX = 0;
+        int startY = 40;
+        int brickWidth = 40;
+        int brickHeight = 40;
+        int rows = 5;
+        int cols = 20;
 
-        for (int r = 0; r < rows; r+=2) {
-            for (int c = 0; c < cols; c+=2) {
+        int normalRate = 70;
+        int powerUpRate = 20;
+        int bonusRate = 10;
+
+        for (int r = 1; r < rows; r++) {
+            for (int c = 1; c < cols - 1; c++) {
+                
                 int x = startX + c * brickWidth;
                 int y = startY + r * brickHeight;
-                Brick brick = new Brick(x, y, brickWidth, brickHeight, 1);
-                brick.setImage(gm.getImage("brick"));
-                bricks.add(brick);
+                Brick brick;
+                int roll = random.nextInt(100);
+
+                if (roll < normalRate) {
+                    brick = new NormalBrick(x, y, brickWidth, brickHeight);
+                    brick.setImage(gm.getImage("brick"));
+                    bricks.add(brick);
+                }
+                else if (roll < normalRate + powerUpRate) {
+                    brick = new PowerUpBrick(x, y, brickWidth, brickHeight);
+                    brick.setImage(gm.getImage("powerup_brick"));
+                    bricks.add(brick);
+                }   
+                else {
+                    brick = new BonusBrick1(x, y, brickWidth, brickHeight);
+                    brick.setImage(gm.getImage("bonus1_brick"));
+                    bricks.add(brick);
+
+                }
+                
+                
             }
 
         }
