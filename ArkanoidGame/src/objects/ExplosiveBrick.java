5a31068051ad3687c6c19e5dcad2624f70ab389c
ancoto1234 <mqan06@gmail.com>
2025-11-01 01:53:36 -0700

    feat: update when the exploding brick is broken, the remaining bricks also disappear.

--- ArkanoidGame/src/objects/ExplosiveBrick.java
+++ ArkanoidGame/src/objects/ExplosiveBrick.java
@@ -3,8 +3,13 @@ package objects;
 
 import core.GameManager;
 import effects.ExplosionEffects;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class ExplosiveBrick extends Brick {
+
+    private final int explosionRadius = 80;
 
 
     public ExplosiveBrick(int x, int y, int width, int height) {
@@ -18,6 +23,32 @@ public class ExplosiveBrick extends Brick {
         //Hiệu ứng nổ sáng
         ExplosionEffects explosion = new ExplosionEffects(x + width / 2, y + height / 2);
         gameManager.addExplosion(explosion);
+
+        List<Brick> toRemove = new ArrayList<>();
+
+        Iterator<Brick> it = gameManager.getBricks().iterator();
+        while (it.hasNext()) {
+            Brick b = it.next();
+            if (b == this) continue; // Bỏ qua chính nó
+
+            // Tính khoảng cách từ tâm viên gạch tới viên gạch bị nổ
+            int bx = b.getX() + b.getWidth() / 2;
+            int by = b.getY() + b.getHeight() / 2;
+
+            double distance = Math.hypot(bx - (x + width / 2), by - (y + height / 2));
+
+
+            // Nếu trong bán kính của vụ nổ => phá
+            if (distance <= explosionRadius) {
+                b.takeHits();
+                if (b.isDestroyed()) {
+                    toRemove.add(b);
+                }
+            
+            }
+        }
+        gameManager.getBricks().removeAll(toRemove);
+        gameManager.getBricks().remove(this);
 
 
     }
